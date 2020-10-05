7220c481f4a955310d8b9414aeb9ce101fcf5987
Alex McDonald <acm4219@gmail.com>
2020-10-05 18:46:15 -0500

    updated the schema to include admin creation

--- employee-Schema.sql
+++ employee-Schema.sql
@@ -38,3 +38,8 @@ department_id int,
 primary key
             (id)
 );
+--Example of a admin creation to create your own user id
+--create user 'ice_admin'@'localhost' identified by 'password';
+-- grant all privileges on ice_creamdb . * to 'ice_admin'@'localhost';
+-- alter user 'ice_admin'@'localhost' identified with mysql_native_password by 'password';
+-- flush privileges;
